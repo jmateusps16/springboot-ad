aa3c024f021d4591e5b1283a30953f065eaf1f22
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-13 15:50:53 -0300

    Removido anotaçoes Spring e criado os construtores

--- src/main/java/com/aimvx/spring/domain/Anime.java
+++ src/main/java/com/aimvx/spring/domain/Anime.java
@@ -1,11 +1,30 @@
 package com.aimvx.spring.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class Anime {
 	private Long id;
 	private String name;
+
+	public void setId (Long id){
+		this.id = id;
+	}
+
+	public Long getID(){
+		return id;
+	}
+
+	public void setName (String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public Anime(){}
+
+	public Anime(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
 }
