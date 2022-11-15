8c987ff78e156dc35ec8dc4c2b77eff3a064d53c
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-14 22:35:08 -0300

    adicionado o Entity na classe

--- src/main/java/com/aimvx/spring/domain/Anime.java
+++ src/main/java/com/aimvx/spring/domain/Anime.java
@@ -1,6 +1,14 @@
 package com.aimvx.spring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Anime {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
