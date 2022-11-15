83aadf0fe2e483fa4b5cd64aaf37ff1404b97fd5
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-15 00:44:29 -0300

    Criado lista para encontrar por nome

--- src/main/java/com/aimvx/spring/repository/AnimeRepository.java
+++ src/main/java/com/aimvx/spring/repository/AnimeRepository.java
@@ -1,8 +1,12 @@
 package com.aimvx.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.aimvx.spring.domain.Anime;
 
 public interface AnimeRepository extends  JpaRepository<Anime, Long>{
+    
+    List<Anime> findByName(String name);
 }
