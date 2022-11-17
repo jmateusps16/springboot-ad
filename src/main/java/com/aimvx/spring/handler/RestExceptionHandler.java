ecc16be892a15d40076e712362c313b3a028cadf
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-17 01:29:13 -0300

    Tratamento de exceptions mais bonitos

--- src/main/java/com/aimvx/spring/handler/RestExceptionHandler.java
+++ src/main/java/com/aimvx/spring/handler/RestExceptionHandler.java
@@ -1,26 +1,35 @@
 package com.aimvx.spring.handler;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.aimvx.spring.exception.BadRequestException;
-import com.aimvx.spring.exception.BadRequestExceptionDetails;
+import com.aimvx.spring.exception.ValidationExceptionDetails;
 
 @ControllerAdvice
 public class RestExceptionHandler {
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationExceptionDetails> handlerMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+        List<FieldError> fieldErrors = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
         return new ResponseEntity<>(
-                BadRequestExceptionDetails.builder()
+            ValidationExceptionDetails.builder()
                     .timestamp(LocalDateTime.now())
                     .status(HttpStatus.BAD_REQUEST.value())
-                    .title("Bad Request Exception, Check the Documentation")
-                    .details(badRequestException.getMessage())
-                    .developerMessage(badRequestException.getClass().getName())
+                    .title("Bad Request Exception, Invalid Fields")
+                    .details("Check the field(s) error")
+                    .developerMessage(methodArgumentNotValidException.getClass().getName())
+                    .fields(fields)
+                    .fieldsMessage(fieldsMessage)
                     .build(), HttpStatus.BAD_REQUEST
                     );
     }
