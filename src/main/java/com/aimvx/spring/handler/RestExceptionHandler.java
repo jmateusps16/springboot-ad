ac4755ed0243ff48ea8a63010445650f753d0939
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-16 23:38:33 -0300

    ExceptionHandler para consumo API

---
+++ src/main/java/com/aimvx/spring/handler/RestExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.aimvx.spring.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.aimvx.spring.exception.BadRequestException;
+import com.aimvx.spring.exception.BadRequestExceptionDetails;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException(BadRequestException badRequestException){
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                    .timestamp(LocalDateTime.now())
+                    .status(HttpStatus.BAD_REQUEST.value())
+                    .title("Bad Request Exception, Check the Documentation")
+                    .details(badRequestException.getMessage())
+                    .developerMessage(badRequestException.getClass().getName())
+                    .build(), HttpStatus.BAD_REQUEST
+                    );
+    }
+
+}
