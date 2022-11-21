667abc8dadc01dacc1034c83169b5afaa6acfa3d
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-20 23:33:07 -0300

    Alerando de lista pra Page

--- src/main/java/com/aimvx/spring/controller/AnimeController.java
+++ src/main/java/com/aimvx/spring/controller/AnimeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -36,9 +38,9 @@ public class AnimeController {
 	private final AnimeService animeService;
 
 	@GetMapping
-	public ResponseEntity<List<Anime>> list() {
+	public ResponseEntity<Page<Anime>> list(Pageable pageable) {
 		log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-		return ResponseEntity.ok(animeService.listAll());
+		return ResponseEntity.ok(animeService.listAll(pageable));
 	}
 
 	@GetMapping(path = "/{id}")
