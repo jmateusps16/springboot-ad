d7e0bbf30e0932224fc2da3a01bcf96957d3e9d4
Mateus Palmeira <jmateusps16@gmail.com>
2022-11-21 23:03:19 -0300

    Diferenças entre postForObject e exchanged

--- src/main/java/com/aimvx/spring/client/SpringClient.java
+++ src/main/java/com/aimvx/spring/client/SpringClient.java
@@ -3,6 +3,7 @@ package com.aimvx.spring.client;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,21 @@ public class SpringClient {
 
         ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange("http://localhost:8080/animes/all", HttpMethod.GET, null, new ParameterizedTypeReference<List<Anime>>() {});
         log.info(exchange);
+
+        /*  
+        Anime kingdom = Anime.builder().name("Kingdom").build();
+        Anime kingdomSaved = new RestTemplate().postForObject("http://localhost:8080/animes/", kingdom, Anime.class);
+        log.info(kingdomSaved);
+        */
+
+        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.POST,
+                new HttpEntity<Anime>(samuraiChamploo),
+                Anime.class);
+        log.info(samuraiChamplooSaved);
+
+
 
     }
 }
